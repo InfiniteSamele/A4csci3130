c5562c98b474f53f0d02fe2db9131d6695a1b358
InfiniteSamele <32623904+InfiniteSamele@users.noreply.github.com>
2018-03-15 23:07:15 -0300

    Update DetailViewActivity.java

--- app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
+++ app/src/main/java/com/acme/a3csci3130/DetailViewActivity.java
@@ -8,30 +8,55 @@ import android.widget.EditText;
 
 public class DetailViewActivity extends Activity {
 
-    private EditText nameField, emailField;
+    private EditText nameField, BnumberField, PField, addressField, provienceField;
     Contact receivedPersonInfo;
+    private MyApplicationData appState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
         receivedPersonInfo = (Contact)getIntent().getSerializableExtra("Contact");
+        appState = ((MyApplicationData) getApplicationContext());
 
         nameField = (EditText) findViewById(R.id.name);
-        emailField = (EditText) findViewById(R.id.email);
+        BnumberField = (EditText) findViewById(R.id.Bnumber);
+        PField = (EditText) findViewById(R.id.Pbusiness);
+        addressField = (EditText) findViewById(R.id.address);
+        provienceField = (EditText) findViewById(R.id.province);
 
         if(receivedPersonInfo != null){
             nameField.setText(receivedPersonInfo.name);
-            emailField.setText(receivedPersonInfo.email);
+            BnumberField.setText(receivedPersonInfo.Bnumber);
+            PField.setText(receivedPersonInfo.Pbusiness);
+            addressField.setText(receivedPersonInfo.address);
+            provienceField.setText(receivedPersonInfo.province);
         }
     }
 
     public void updateContact(View v){
         //TODO: Update contact funcionality
+        String personID = receivedPersonInfo.uid;
+        String name = nameField.getText().toString();
+        String num = BnumberField.getText().toString();
+        String Pbus = PField.getText().toString();
+        String add = addressField.getText().toString();
+        String pro = provienceField.getText().toString();
+
+        Contact person = new Contact(personID, num, name, Pbus, add, pro);
+
+        appState.firebaseReference.child(personID).setValue(person);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 
     public void eraseContact(View v)
     {
         //TODO: Erase contact functionality
+        appState.firebaseReference.child(receivedPersonInfo.uid).removeValue();
+        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
