7ed88da0b2def07b4610118e6d8c2e83e21ef1f4
InfiniteSamele <32623904+InfiniteSamele@users.noreply.github.com>
2018-03-15 23:05:55 -0300

    Update Contact.java

--- app/src/main/java/com/acme/a3csci3130/Contact.java
+++ app/src/main/java/com/acme/a3csci3130/Contact.java
@@ -15,26 +15,38 @@ import java.util.Map;
 public class Contact implements Serializable {
 
     public  String uid;
+    public  String uid;
     public  String name;
-    public  String email;
+    public  String Bnumber;
+    public  String Pbusiness;
+    public  String address;
+    public  String province;
+
 
     public Contact() {
         // Default constructor required for calls to DataSnapshot.getValue
     }
 
-    public Contact(String uid, String name, String email){
+    public Contact(String uid, String name, String Bnumber, String Pbussiness, String address, String province){
         this.uid = uid;
         this.name = name;
-        this.email = email;
+        this.Bnumber = Bnumber;
+        this.Pbusiness = Pbussiness;
+        this.address = address;
+        this.province = province;
     }
 
     @Exclude
     public Map<String, Object> toMap(){
         HashMap<String, Object> result = new HashMap<>();
-        result.put("uid", uid);
-        result.put("name", name);
-        result.put("email", email);
+        result.put("Bussiness number:", Bnumber);
+        result.put("Name:", name);
+        result.put("Primary business:", Pbusiness);
+        result.put("Address:", address);
+        result.put("Province/territory:", province);
+
 
         return result;
     }
 }
+
